1ae493678601762e571f7730ceecd5c89cd2e375
Au2348 <aurelie.m@tutamail.com>
2026-09-24 18:39:59 +0200

    configuration Vitest + Testing Library

---
+++ UNO_REACT/src/__tests__/setup.ts
@@ -0,0 +1,6 @@
+/**
+ * Setup pour les tests Vitest + Testing Library.
+ * Importe les matchers personnalisés de jest-dom
+ * (toBeInTheDocument, toHaveTextContent, etc.)
+ */
+import "@testing-library/jest-dom/vitest";
